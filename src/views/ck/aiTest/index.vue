eb2344f3c6a93a1d2c8d36404b0f6855ac60bdf4
Yan <3534358648@qq.com>
2025-11-23 16:31:03 +0800

    ai

---
+++ src/views/ck/aiTest/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <i-frame :src="url"></i-frame>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const url = ref('http://localhost:8081/chatui/index.html')
+</script>
